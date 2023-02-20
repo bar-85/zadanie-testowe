4a1839e121289bb2b409b2d85ee516e7941c8a58
bar-85 <bajwolskabarbara@gmail.com>
2023-02-20 17:20:34 +0100

    Add Title.jsx and style

---
+++ src/components/testA/Title.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Title.css'
+
+const Title = () => {
+	return (
+		<div className='title'>
+			<div className='title_border'>
+				<div className='title_up'>
+					<p>How It Work</p>
+				</div>
+				<div className='title_middle'>
+					<p>A Simple, Proven Way to Boost Your Team Performance.</p>
+				</div>
+				<div className='title_bottom'>
+					<p>Most popular type of partnership Malta. The limited liability is, in fact, the only type of company allowed by Companies.</p>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Title
